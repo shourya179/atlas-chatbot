769bf94a4b51e9cdf8163aedf70bb61bb5aab3b8
Shourya179 <shouryavardhan81@gmail.com>
2026-02-04 14:29:37 +0530

    Add chat-stream endpoint for streaming responses and improve UI interactions

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 from core.bot import chatbot
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
+import time
 
 app = FastAPI()
 bot = chatbot("Atlas")
@@ -32,3 +34,22 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.post("chat-stream")
+
+def chat_stream(request:dict):
+    user_input = request.get("massage","")
+
+    def event_gentrator():
+        # 🔴 TEMP: simulate streaming
+        # Later this will come from the LLM
+        response = bot.handle(user_input)
+
+        for word in response.split():
+            yield f"data:{word}\n\n"
+            time.sleep(.05)
+        yield "data:[END]\n\n"
+    return StreamingResponse(
+        event_gentrator(),
+        media_type="text/event-steam"
+    )
